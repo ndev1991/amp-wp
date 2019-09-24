1fe4ebc56bfeacaa9a4e0d86f46795c8eda1e051
Jonny Harris <jon@spacedmonkey.co.uk>
2019-09-24 15:58:56 +0100

    Use material UI colour picker.

--- assets/src/stories-editor/blocks/amp-story-page/background-color-settings.js
+++ assets/src/stories-editor/blocks/amp-story-page/background-color-settings.js
@@ -3,17 +3,15 @@
  */
 import PropTypes from 'prop-types';
 import { ReactElement } from 'react';
+import ColorPicker from 'material-ui-color-picker';
 
 /**
  * WordPress dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
+
 import {
-	PanelColorSettings,
-} from '@wordpress/block-editor';
-import {
-	Button,
-	RangeControl,
+	PanelBody,
 } from '@wordpress/components';
 
 /**
@@ -69,37 +67,19 @@ const BackgroundColorSettings = ( { backgroundColors, setAttributes, overlayOpac
 	};
 
 	return (
-		<PanelColorSettings
+		<PanelBody
+			className="amp-story-order-controls"
 			title={ __( 'Background Color', 'amp' ) }
-			colorSettings={ getOverlayColorSettings() }
 		>
-			<p>
-				{ backgroundColors.length < 2 &&
-				<Button
-					onClick={ () => setBackgroundColors( null, 1 ) }
-					isSmall>
-					{ __( 'Add Gradient', 'amp' ) }
-				</Button>
-				}
-				{ backgroundColors.length > 1 &&
-				<Button
-					onClick={ () => removeBackgroundColor( backgroundColors.length - 1 ) }
-					isLink
-					isDestructive>
-					{ __( 'Remove Gradient', 'amp' ) }
-				</Button>
-				}
-			</p>
-			<RangeControl
-				label={ __( 'Opacity', 'amp' ) }
-				value={ overlayOpacity }
-				onChange={ ( value ) => setAttributes( { overlayOpacity: value } ) }
-				min={ 0 }
-				max={ 100 }
-				step={ 5 }
-				required
+			<ColorPicker
+				name='color'
+				defaultValue='#000'
+				// value={this.state.color} - for controlled component
+				onChange={color => setBackgroundColors(color, 1)}
+
 			/>
-		</PanelColorSettings>
+		</PanelBody>
+
 	);
 };
 
